c2fa18729f9c49c044868bf4ae1d7204139189db
Stratus3D <admin@stratus3d.com>
2012-04-04 17:06:03 -0400

    Got sitemap displayed. Need to figure out how to call jquery UI popup

--- single_pages/dashboard/sitemap/sort_subpages.php
+++ single_pages/dashboard/sitemap/sort_subpages.php
@@ -10,12 +10,14 @@ CCM_LAUNCHER_SITEMAP = 'sort_subpages'; // we need this for when we are moving a
 	
 		<div class="ccm-pane-options" id="ccm-<?php echo $searchInstance?>-pane-options">
 		</div>
-	
+        <div class="ccm-pane-body ccm-pane-body-footer">
+        <?php $pageSelector = Loader::helper('form/page_selector');
+        print $pageSelector->sitemap(); ?>
+        </div>
 	<?php  } else { ?>
-		<div class="ccm-pane-body">
-			<p><?php echo t("You must have access to the dashboard sitemap to search pages.")?></p>
-		</div>	
-	
+		<div class="ccm-pane-body ccm-pane-body-footer">
+			<p><?php echo t("You must have access to the dashboard sitemap to search pages.")?></p>	
+    </div>	
 	<?php  } ?>
 
 <?php echo Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(false); ?>
